be79789454d7d76c3bd0c9035e1018ee65026830
Alexander-2049 <a.alexander.2049@gmail.com>
2025-12-31 02:28:17 +0200

    updated routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { ArchivedRoomViewComponent } from './archived-room-view/archived-room-view.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +12,14 @@ export const routes: Routes = [
   {
     path: 'rooms/:id',
     component: ChatWindowComponent,
+  },
+  {
+    path: 'archive',
+    component: ArchiveComponent,
+  },
+  {
+    path: 'archive/:id',
+    component: ArchivedRoomViewComponent,
   },
   {
     path: '',
