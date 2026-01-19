b908029e43a3155be2ed7f5c9b558ba3ba3a1da3
Alexander Akhilchenok <121698003+Alexander-2049@users.noreply.github.com>
2026-01-19 07:11:46 +0200

    Fix frontend unit tests

--- frontend/src/app/chat-window/chat-window.component.spec.ts
+++ frontend/src/app/chat-window/chat-window.component.spec.ts
@@ -1,6 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, of } from 'rxjs';
 import { ChatWindowComponent } from './chat-window.component';
+import { WebSocketService } from '../services/websocket.service';
+import { UserService } from '../services/user.service';
+
+class MockWebSocketService {
+  getMessagesOfType = <T>() => new Subject<T>().asObservable();
+  leaveRoom = () => undefined;
+  joinRoom = () => undefined;
+  archiveRoom = () => undefined;
+  sendMessage = () => undefined;
+}
+
+class MockUserService {
+  getNickname = () => null;
+  getUserId = () => null;
+  setNickname = () => undefined;
+}
+
+class MockRouter {
+  events = new Subject();
+  getCurrentNavigation = () => null;
+  navigate = () => Promise.resolve(true);
+}
 
 describe('ChatWindowComponent', () => {
   let component: ChatWindowComponent;
@@ -9,6 +32,12 @@ describe('ChatWindowComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ChatWindowComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({ id: '1' }) } },
+        { provide: WebSocketService, useClass: MockWebSocketService },
+        { provide: UserService, useClass: MockUserService },
+        { provide: Router, useClass: MockRouter },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ChatWindowComponent);
